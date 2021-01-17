f4c2e1ed1781fb0d36701553b67de8bbf6f5bc34
ccachoulet <clementcachoulet@orange.fr>
2021-01-17 23:47:05 +0100

    add mongoDB database

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 require('dotenv').config({path: './config/.env'})
+require('./config/db')
 const app = express();
 
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT ,() => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
